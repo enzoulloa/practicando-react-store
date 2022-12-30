27c79c9f158ac749ad525d761c44b2d0de2575e3
enzoulloa <enzo_ulloa.i@gmail.com>
2022-12-30 16:28:47 -0300

    feat: :fire: add Layout, add Header, add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p className='Footer-title'>Store</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h2 className='Header-title' />
+      <div className='Header-checkout'>
+        Chekout
+      </div>
+    </div>
+  )
+}
+
+export default Header
