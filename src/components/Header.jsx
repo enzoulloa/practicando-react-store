22e688efd70f8f79c936c2875e840e0f8d57b716
enzoulloa <enzo_ulloa.i@gmail.com>
2022-12-30 20:35:05 -0300

    feat: :sparkles: update header button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 import '../styles/components/Header.css'
 
 const Header = () => {
+  const { state } = useContext(AppContext)
+  const { cart } = state
   return (
     <div className='Header'>
       <h2 className='Header-title' />
@@ -10,6 +14,7 @@ const Header = () => {
         <Link to='/checkout'>
           <i className='fas fa-shopping-basket fa-2x' />
         </Link>
+        {cart.length > 0 && <div className='Header-alert'>{cart.length}</div>}
       </div>
     </div>
   )
