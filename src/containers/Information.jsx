a759bc77124d4686779b84a0982eea58869ef5c4
enzoulloa <enzo_ulloa.i@gmail.com>
2022-12-30 21:18:27 -0300

    feat: :sparkles: add interactivity-information

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,7 +1,33 @@
-import { Link } from 'react-router-dom'
+import { useRef, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 import '../styles/components/Information.css'
 
 const Information = () => {
+  const { state, addToBuyer } = useContext(AppContext)
+  const form = useRef(null)
+  const navigate = useNavigate()
+
+  const { cart } = state
+
+  const handleSubmit = () => {
+    // eslint-disable-next-line no-undef
+    const formData = new FormData(form.current)
+    const buyer = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      address: formData.get('address'),
+      apto: formData.get('apto'),
+      city: formData.get('city'),
+      country: formData.get('country'),
+      state: formData.get('state'),
+      cp: formData.get('cp'),
+      phone: formData.get('phone')
+    }
+    addToBuyer(buyer)
+    navigate('checkout/payment')
+  }
+
   return (
     <div className='Information'>
       <div className='Information-content'>
@@ -9,7 +35,7 @@ const Information = () => {
           <h2>Informacion de contacto</h2>
         </div>
         <div className='Information-form'>
-          <form action=''>
+          <form ref={form}>
             <input type='text' placeholder='Nombre completo' name='name' />
             <input type='text' placeholder='Correo Eletronico' name='email' />
             <input type='text' placeholder='Direccion' name='address' />
@@ -22,21 +48,23 @@ const Information = () => {
         </div>
         <div className='Information-buttons'>
           <div className='Information-back'>
-            Regresar
+            <Link to='/checkout'>Regresar</Link>
           </div>
           <div className='Information-next'>
-            <Link to='/checkout/payment'>Pagar</Link>
+            <button type='button' onClick={handleSubmit}>Pagar</button>
           </div>
         </div>
       </div>
       <div className='Information-sidebar'>
         <h3>Pedido:</h3>
-        <div className='Information-item'>
-          <div className='Information-element'>
-            <h4>Item Name</h4>
-            <span>$10</span>
+        {cart.map(item => (
+          <div key={item.id} className='Information-item'>
+            <div className='Information-element'>
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
